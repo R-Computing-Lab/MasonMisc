f49c3ef626a52572d440f1dcc01f7158552ec33e
Mason Garrison <s.mason.garrison@gmail.com>
2020-03-23 08:08:42 -0400

    Update syndata.R

--- R/syndata.R
+++ R/syndata.R
@@ -20,18 +20,20 @@ syndata <- function(df=NULL,
   if(is.null(df)&is.null(cov.mat)){
     stop("You must provide either a data frame or a covariance matrix for this function to work")}
   if(!is.null(df)){ #has data frame
-    cov.mat= stats::cov(df)
-    means=Matrix::colMeans(df)
+    cov.mat= stats::cov(df,use = "na.or.complete")
+    means=Matrix::colMeans(df,na.rm=TRUE)
+    if(is.null(varnames)){
     varnames=names(df)
+    }
   }
   if(is.null(means)){ #missing means
-        means=rep(0,ncol(cov.mat))
+    means=rep(0,ncol(cov.mat))
   }
 
   data_sim=as.data.frame(MASS::mvrnorm(
-      n,
-      means,
-      cov.mat))
+      n=n,
+      mu=means,
+      Sigma=cov.mat))
 
   if(!is.null(varnames)){
     names(data_sim)=varnames
